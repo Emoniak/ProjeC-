2887b8b248676021dc62cbe954679496838e648a
Antoine BELLENOUE <antoine.bellenoue@viacesi.fr>
2018-10-31 16:19:38 +0100

    Debut projet

---
+++ WindowsForms/Login.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsForms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.TbDb = new System.Windows.Forms.TextBox();
+            this.TbMdp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mot de passe";
+            // 
+            // Submit
+            // 
+            this.Submit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Submit.Location = new System.Drawing.Point(15, 185);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(77, 36);
+            this.Submit.TabIndex = 2;
+            this.Submit.Text = "Login";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cancel.Location = new System.Drawing.Point(129, 185);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(77, 36);
+            this.Cancel.TabIndex = 3;
+            this.Cancel.Text = "Anuler";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // TbDb
+            // 
+            this.TbDb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbDb.Location = new System.Drawing.Point(98, 77);
+            this.TbDb.Name = "TbDb";
+            this.TbDb.Size = new System.Drawing.Size(123, 20);
+            this.TbDb.TabIndex = 4;
+            // 
+            // TbMdp
+            // 
+            this.TbMdp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbMdp.Location = new System.Drawing.Point(98, 133);
+            this.TbMdp.Name = "TbMdp";
+            this.TbMdp.PasswordChar = '*';
+            this.TbMdp.Size = new System.Drawing.Size(123, 20);
+            this.TbMdp.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 252);
+            this.Controls.Add(this.TbMdp);
+            this.Controls.Add(this.TbDb);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.TextBox TbDb;
+        private System.Windows.Forms.TextBox TbMdp;
+    }
+}
+
---
+++ WindowsForms/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            SQL.DB = TbDb.Text;
+            SQL.MDP = TbMdp.Text;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(SQL.ConnexionString()))
+                {
+                    conn.Open();
+                    conn.Close();
+                }
+                Form1 f = new Form1();
+                this.Visible = false;
+                f.ShowDialog();
+                this.Visible = true;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
