52aed9772a4e0018e21b9528b64948c7d7371120
Antoine BELLENOUE <antoine.bellenoue@outlook.com>
2018-11-19 11:05:14 +0100

    creation ds objet standard en dll

--- librairyOutil/Vehicule.cs
+++ librairyOutil/Vehicule.cs
@@ -8,6 +8,45 @@ namespace librairyOutil
 {
     public class Vehicule
     {
+        private int nbRoue;
+        private object[] options;
+        private string marque;
+        private string  model;
+        private int plaque;
+
+        public int Plaque
+        {
+            get { return plaque; }
+            set { plaque = value; }
+        }
+
+
+        public string  Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+
+        public string Marque
+        {
+            get { return marque; }
+            set { marque = value; }
+        }
+
+
+        public object[] Options
+        {
+            get { return options; }
+            set { options = value; }
+        }
+
+
+        public int NbRoue
+        {
+            get { return nbRoue; }
+            set { nbRoue = value; }
+        }
 
     }
 }
