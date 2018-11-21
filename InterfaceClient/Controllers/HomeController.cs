e0c8a5e4dd8cae00826ec9fa60f69beaacbb401c
ReVaStar <xgamev05@gmail.com>
2018-11-21 16:07:40 +0100

    Base Site

---
+++ InterfaceClient/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterfaceClient
+{
+    public class HomeController : Controller
+    {
+        public static string cs = @"Server=mysql-serveur.mysql.database.azure.com; Port=3306; Database=db_garage; Uid=AdminGarage@mysql-serveur; Pwd=TOTO1234!; SslMode=Preferred;";
+        // GET: Home
+        public ActionResult Index()
+        {
+            DataVehicule data = new DataVehicule();
+            data.getVehicules();
+            return View(data);
+        }
+    }
+}
---
+++ InterfaceClient/Models/DataVehicule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace InterfaceClient
+{
+    public class DataVehicule
+    {
+        public struct vehicule
+        {
+            public string marque { get; set; }
+            public string model { get; set; }
+        };
+
+        public List<vehicule> Vehicules = new List<vehicule>();
+
+        public void getVehicules()
+        {
+            using (MySqlConnection conn = new MySqlConnection(HomeController.cs))
+            {
+                
+                conn.Open();
+                MySqlCommand command = new MySqlCommand(@"select nom_marque from tmarque", conn);
+                MySqlDataReader dr = command.ExecuteReader();
+                while(dr.Read())
+                {
+                    Vehicules.Add(new vehicule { marque = dr[0].ToString(),model="" });
+                }
+                conn.Close();
+            }
+        }
+    }
+}
