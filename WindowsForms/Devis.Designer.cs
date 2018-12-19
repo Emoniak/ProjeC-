8cc11611ecc59555869f532aaa009ef01c7dc10d
valentinval1 <valentin.maillardd@gmx.fr>
2018-12-19 09:37:40 +0100

    cpmmit beffore pull

--- WindowsForms/Devis.Designer.cs
+++ WindowsForms/Devis.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PasserFacture = new System.Windows.Forms.Button();
+            this.details = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 4);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(252, 328);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // PasserFacture
+            // 
+            this.PasserFacture.Location = new System.Drawing.Point(259, 275);
+            this.PasserFacture.Name = "PasserFacture";
+            this.PasserFacture.Size = new System.Drawing.Size(126, 57);
+            this.PasserFacture.TabIndex = 1;
+            this.PasserFacture.Text = "Payer";
+            this.PasserFacture.UseVisualStyleBackColor = true;
+            this.PasserFacture.Click += new System.EventHandler(this.PasserFacture_Click);
+            // 
+            // details
+            // 
+            this.details.Location = new System.Drawing.Point(259, 211);
+            this.details.Name = "details";
+            this.details.Size = new System.Drawing.Size(126, 58);
+            this.details.TabIndex = 2;
+            this.details.Text = "Détails";
+            this.details.UseVisualStyleBackColor = true;
+            this.details.Click += new System.EventHandler(this.details_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(395, 4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(402, 111);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(395, 111);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(402, 221);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // Devis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 344);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.details);
+            this.Controls.Add(this.PasserFacture);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Devis";
             this.Text = "Devis";
+            this.Load += new System.EventHandler(this.Devis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button PasserFacture;
+        private System.Windows.Forms.Button details;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
